7cb801efbbc3fba2e04bc38f28a6dd34b74d6d24
YuChocopie <rladbwjd341@naver.com>
2019-06-29 00:07:46 +0900

    inital commit

---
+++ app/src/main/java/com/yojeumgeosdeul/dong/MyPageActivity.java
@@ -0,0 +1,12 @@
+package com.yojeumgeosdeul.dong;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
